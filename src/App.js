8e320483e8d2d55673ae94e602af4b10c387c99e
baronasalex <baronasalex@gmail.com>
2022-10-14 16:22:02 +0300

    "First commit in ToDoList"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from './Table';
+import { Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+        age: '21',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+        age: '28',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+        age: '36',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+        age: '26',
+      },
+      {
+        name: 'Alex',
+        job: 'Programmer',
+        age: '29',
+      },
+    ]
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) =>{
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state
+
+    return (
+      <div className="container">
+        <h1 className="text-center mt-4 mb-4">To Do List</h1>
+        <Table 
+        characterData={characters} 
+        removeCharacter={this.removeCharacter}/>
+        <div id className="d-grid gap-2">
+      <Button variant="primary" size="lg">
+        Block button
+      </Button>
+      </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Table.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {Table, Button} from 'react-bootstrap';
+
+const TableHeader = () => {
+    return (
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    )
+  }
+  
+
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index, age) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>{row.age}</td>
+                <td>
+                    <Button variant="danger"
+                    onClick={() => props.removeCharacter(index)}>Delete</Button>
+                </td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
+}
+
+const TableMain = (props) => {
+    const {characterData, removeCharacter} = props
+
+    return (
+        <Table striped bordered hover>
+          <TableHeader />
+          <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+        </Table >
+      )
+}
+
+export default TableMain
